ced387728733a6893219c0ec10e4068744964f01
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-17 13:57:13 -0600

    Move terraform to deployments folder

--- deployments/lambda.tf
+++ deployments/lambda.tf
@@ -15,8 +15,14 @@ resource "aws_lambda_function" "add-category" {
   s3_bucket = "life-track-serverless"
   s3_key = "v1.0.0/add-category"
   handler = "main"
-  role = aws_iam_role.lambda-exec-write-db.arn
+  role = aws_iam_role.lambda-exec-full-db.arn
   runtime = "go1.x"
+  environment {
+    variables = {
+      "LT_TABLE_NAME": aws_dynamodb_table.lt-category.name,
+      "LT_TABLE_REGION": "us-east-1"
+    }
+  }
 }
 
 data "aws_iam_policy_document" "lambda-dynamo-read" {
@@ -45,7 +51,31 @@ data "aws_iam_policy_document" "lambda-dynamo-write" {
   }
 }
 
+data "aws_iam_policy_document" "lambda-dynamo-full" {
+  statement {
+    actions = ["sts:AssumeRole", "dynamodb:PutItem",
+      "dynamodb:DeleteItem", "dynamodb:UpdateItem", "dynamodb:GetItem",
+      "dynamodb:Scan", "dynamodb:Query"]
+    resources = [aws_dynamodb_table.lt-category.arn]
+    effect = "Allow"
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type = ""
+    }
+  }
+}
+
 resource "aws_iam_role" "lambda-exec-write-db" {
   name = "lt-lambda-write-db"
   assume_role_policy = data.aws_iam_policy_document.lambda-dynamo-write.json
+}
+
+resource "aws_iam_role" "lambda-exec-read-db" {
+  name = "lt-lambda-read-db"
+  assume_role_policy = data.aws_iam_policy_document.lambda-dynamo-read.json
+}
+
+resource "aws_iam_role" "lambda-exec-full-db" {
+  name = "lt-lambda-full-db"
+  assume_role_policy = data.aws_iam_policy_document.lambda-dynamo-full.json
 }
