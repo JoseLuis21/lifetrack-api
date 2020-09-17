937d3e51981718e65ce979ffc194823c97ca666b
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-17 14:43:49 -0600

    Update build shell scripts, now using GOPATH env variable

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-  GOOS=linux go build -o ./bin/"$1" ../cmd/"$1"/main.go
-  zip ./release/"$1".zip ./bin/"$1"
+  LT_PATH="$GOPATH/src/github.com/neutrinocorp/life-track-api"
+  GOOS=linux go build -o "$LT_PATH"/build/bin/"$1" "$LT_PATH"/cmd/"$1"/main.go
+  zip "$LT_PATH"/build/release/"$1".zip "$LT_PATH"/build/bin/"$1"
   else
     echo "empty argument"
 fi
--- build/deploy-to-s3.sh
+++ build/deploy-to-s3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-  /bin/bash ./build.sh "$1"
-  aws s3 cp ./release/"$1".zip s3://life-track-serverless/v1.0.0/"$1".zip
+  LT_PATH="$GOPATH/src/github.com/neutrinocorp/life-track-api"
+  /bin/bash "$LT_PATH"/build/build.sh "$1"
+  aws s3 cp "$LT_PATH"/build/release/"$1".zip s3://life-track-serverless/v1.0.0/"$1".zip
   else
     echo "empty argument"
 fi
