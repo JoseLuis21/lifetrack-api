c9a44ae9ead245ac1a92e8d7816e9089462dc4b5
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-18 00:40:26 -0600

    Remove resource proxy from API Gateway

--- deployments/api_gateway.tf
+++ deployments/api_gateway.tf
@@ -53,29 +53,40 @@ resource "aws_route53_record" "domain-53" {
 
 // -- API Gateway Proxy config --
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "category" {
   rest_api_id = aws_api_gateway_rest_api.lifeTrack.id
   parent_id = aws_api_gateway_rest_api.lifeTrack.root_resource_id
-  path_part = "{proxy+}"
+  path_part = "category"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "category_post" {
   rest_api_id = aws_api_gateway_rest_api.lifeTrack.id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = "ANY"
+  resource_id = aws_api_gateway_resource.category.id
+  http_method = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.lifeTrack.id
-  resource_id = aws_api_gateway_method.proxy.resource_id
-  http_method = aws_api_gateway_method.proxy.http_method
+  resource_id = aws_api_gateway_method.category_post.resource_id
+  http_method = aws_api_gateway_method.category_post.http_method
 
   integration_http_method = "POST"
   type = "AWS_PROXY"
   uri = aws_lambda_function.add-category.invoke_arn
 }
 
+/*
+INFO: Avoid using proxy to keep serverless ecosystem
+
+resource "aws_api_gateway_resource" "category_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.lifeTrack.id
+  parent_id = aws_api_gateway_resource.category.id
+  path_part = "{proxy+}"
+}
+*/
+
+/*
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id = aws_api_gateway_rest_api.lifeTrack.id
   resource_id = aws_api_gateway_rest_api.lifeTrack.root_resource_id
@@ -92,11 +103,10 @@ resource "aws_api_gateway_integration" "lambda_root" {
   type = "AWS_PROXY"
   uri = aws_lambda_function.add-category.invoke_arn
 }
-
+*/
 resource "aws_api_gateway_deployment" "deploy" {
   depends_on = [
-    aws_api_gateway_integration.lambda,
-    aws_api_gateway_integration.lambda_root
+    aws_api_gateway_integration.lambda
   ]
   rest_api_id = aws_api_gateway_rest_api.lifeTrack.id
   stage_name = "live"
