34d2f94aefd5a538dd98bce922b29a5fbd7062ca
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-17 23:12:29 -0600

    Add versioning to shell script

--- build/deploy-to-s3.sh
+++ build/deploy-to-s3.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+APP_VERSION="v1.0.0"
+
+if [ "$2" != "" ]; then
+    APP_VERSION=v"$2"
+fi
+
 if [ "$1" != "" ]; then
   LT_PATH="$GOPATH/src/github.com/neutrinocorp/life-track-api"
   /bin/bash "$LT_PATH"/build/build.sh "$1"
-  aws s3 cp "$LT_PATH"/build/release/"$1".zip s3://life-track-serverless/v1.0.0/"$1".zip
+  aws s3 cp "$LT_PATH"/build/release/"$1".zip s3://life-track-serverless/"$APP_VERSION"/"$1".zip
   else
     echo "empty argument"
 fi
