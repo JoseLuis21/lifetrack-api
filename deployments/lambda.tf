c649e1ad978b31a4c34d5af03a8a8c44fdffa6cb
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-17 23:14:45 -0600

    Add lambda versioning

--- deployments/lambda.tf
+++ deployments/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "add-category" {
   function_name = "lt-add-category"
   s3_bucket = "life-track-serverless"
-  s3_key = "v1.0.0/add-category.zip"
+  s3_key = "v${var.app_version}/add-category.zip"
   handler = "add-category"
   role = aws_iam_role.category-lambda-exec-full-db.arn
   timeout = 15
@@ -12,4 +12,20 @@ resource "aws_lambda_function" "add-category" {
       "LT_TABLE_REGION": "us-east-1"
     }
   }
+  tags = {
+    Environment: "prod",
+    Name: "neutrino-lifetrack"
+  }
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.add-category.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.lifeTrack.execution_arn}/*/*"
+}
+
+variable "app_version" {
 }
