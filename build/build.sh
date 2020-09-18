df3798b3fd7d9a265c8f2c3f6a4ee2ce716a4e2c
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-17 19:11:31 -0600

    Fix build script zip nested directories

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" != "" ]; then
   LT_PATH="$GOPATH/src/github.com/neutrinocorp/life-track-api"
-  GOOS=linux go build -o "$LT_PATH"/build/bin/"$1" "$LT_PATH"/cmd/"$1"/main.go
-  zip "$LT_PATH"/build/release/"$1".zip "$LT_PATH"/build/bin/"$1"
+  GOOS=linux GOARCH=amd64 go build -o "$LT_PATH"/build/bin/"$1" "$LT_PATH"/cmd/"$1"/main.go
+  zip -D -j "$LT_PATH"/build/release/"$1".zip "$LT_PATH"/build/bin/"$1"
   else
     echo "empty argument"
 fi
