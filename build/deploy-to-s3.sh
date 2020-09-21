b262fd28247df6e9610f8d90e8cbcf379a0cf533
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-21 17:05:14 -0600

    Refactor deploy-to-s3 shell script, using functional programming

--- build/deploy-to-s3.sh
+++ build/deploy-to-s3.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
+#
+# Perform Go binary build for Amazon Linux/Linux OS based distros and subsequently upload it to AWS S3
+# Requires AWS CLi and proper configured credentials
 
-APP_VERSION="v1.0.0"
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
 
-if [ "$2" != "" ]; then
-    APP_VERSION=v"$2"
-fi
+#######################################
+# Run build.sh executable shell script and upload output compressed file to AWS S3
+# Important Note: Requires AWS CLi and proper credentials
+# Globals:
+#   GOPATH
+# Arguments:
+#   $1 = Module name (e.g. add-category)
+#   $2 = Application version (e.g. 1.0.1)
+#     Uses v1.0.0 as default value
+#   $3 = AWS S3 bucket name (e.g. my-lifetrack-bucket)
+#     Uses lifetrack-serverless as default value
+#######################################
+function upload_to_s3() {
+  APP_VERSION="v1.0.0"
+  # Legacy bucket name - life-track-serverless
+  BUCKET_NAME="lifetrack-serverless"
 
-if [ "$1" != "" ]; then
-  LT_PATH="$GOPATH/src/github.com/neutrinocorp/life-track-api"
-  /bin/bash "$LT_PATH"/build/build.sh "$1"
-  aws s3 cp "$LT_PATH"/build/release/"$1".zip s3://life-track-serverless/"$APP_VERSION"/"$1".zip
-  else
-    echo "empty argument"
-fi
+  if [ "$2" != "" ]; then
+      APP_VERSION=v"$2"
+  fi
+
+  if [ "$3" != "" ]; then
+      BUCKET_NAME="$3"
+  fi
+
+  if [ "$1" != "" ]; then
+    LT_PATH="$GOPATH/src/github.com/neutrinocorp/life-track-api"
+    /bin/bash "$LT_PATH"/build/build.sh "$1"
+    aws s3 cp "$LT_PATH"/build/release/"$1".zip s3://"$BUCKET_NAME"/"$APP_VERSION"/"$1".zip
+    else
+      err "empty argument"
+      exit 1
+  fi
+}
+
+upload_to_s3 "$1" "$2" "$3"
