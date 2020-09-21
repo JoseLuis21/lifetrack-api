d144be404bc98d403014429daa4b07d8bf729e93
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-20 23:37:48 -0600

    Update add category dependency injection

--- cmd/add-category/main.go
+++ cmd/add-category/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"github.com/alexandria-oss/common-go/exception"
 	"github.com/alexandria-oss/common-go/httputil"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -17,15 +18,15 @@ import (
 var muxLambda *gorillamux.GorillaMuxAdapter
 
 func init() {
+	cmd, err := dep.InjectAddCategoryHandler()
+	if err != nil {
+		log.Fatalf("failed to start add category command: %s", exception.GetDescription(err))
+		return
+	}
+
 	log.Print("mux cold start")
 	r := mux.NewRouter()
 	r.Path("/live/category").Methods(http.MethodPost).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cmd, err := dep.InjectAddCategoryHandler()
-		if err != nil {
-			httputil.RespondErrorJSON(err, w)
-			return
-		}
-
 		err = cmd.Handle(command.AddCategory{
 			Ctx:         r.Context(),
 			Title:       r.PostFormValue("title"),
