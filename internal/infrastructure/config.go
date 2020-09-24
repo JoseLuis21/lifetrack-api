5f1e6867805f391baacdb5d911a95b8d9ff5c359
A Ruiz <luis.alonso.16@hotmail.com>
2020-09-23 23:40:43 -0600

    Update configuration

--- internal/infrastructure/config.go
+++ internal/infrastructure/config.go
@@ -3,36 +3,59 @@ package infrastructure
 import "github.com/spf13/viper"
 
 type Configuration struct {
-	Table struct {
-		Name   string
-		Region string
+	Category struct {
+		Table struct {
+			Name   string
+			Region string
+		}
+		Event struct {
+			Region string
+		}
 	}
 }
 
 func NewConfiguration() (Configuration, error) {
-	viper.SetDefault("table_name", "lt-category")
-	viper.SetDefault("table_region", "us-east-1")
+	viper.SetDefault("category.table.name", "lt-category")
+	viper.SetDefault("category.table.region", "us-east-1")
+	viper.SetDefault("category.event.region", "us-east-1")
 
 	if err := SetOSEnv(); err != nil {
 		return Configuration{}, err
 	}
 
 	return Configuration{
-		Table: struct {
-			Name   string
-			Region string
+		Category: struct {
+			Table struct {
+				Name   string
+				Region string
+			}
+			Event struct {
+				Region string
+			}
 		}{
-			Name:   viper.GetString("table_name"),
-			Region: viper.GetString("table_region"),
+			Table: struct {
+				Name   string
+				Region string
+			}{
+				Name:   viper.GetString("category.table.name"),
+				Region: viper.GetString("category.table.region"),
+			},
+			Event: struct {
+				Region string
+			}{
+				Region: viper.GetString("category.event.region"),
+			},
 		},
 	}, nil
 }
 
 func SetOSEnv() error {
 	viper.SetEnvPrefix("lt")
-	if err := viper.BindEnv("table_name"); err != nil {
+	if err := viper.BindEnv("category_table_name", "category.table.name"); err != nil {
+		return err
+	} else if err := viper.BindEnv("category_event_region", "category.event.region"); err != nil {
 		return err
 	}
 
-	return viper.BindEnv("table_region")
+	return viper.BindEnv("category_table_region", "category.table.region")
 }
