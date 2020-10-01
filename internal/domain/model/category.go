3969c7157f9d6ff45b382c085c47664e3409bb1d
Alonso R <luis.alonso.16@hotmail.com>
2020-09-30 23:13:49 -0600

    Update query model

--- internal/domain/model/category.go
+++ internal/domain/model/category.go
@@ -1,12 +1,37 @@
 package model
 
+import (
+	"encoding/json"
+	"github.com/alexandria-oss/common-go/exception"
+)
+
 // Category read model
 type Category struct {
 	ID          string `json:"category_id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	User        string `json:"user"`
+	Theme       string `json:"theme"`
 	CreateTime  int64  `json:"create_time"`
 	UpdateTime  int64  `json:"update_time"`
 	Active      bool   `json:"active"`
+}
+
+// MarshalBinary converts current model to binary data (JSON)
+func (c Category) MarshalBinary() ([]byte, error) {
+	cJSON, err := json.Marshal(c)
+	if err != nil {
+		return nil, exception.NewFieldFormat("category aggregate", "json")
+	}
+
+	return cJSON, nil
+}
+
+// UnmarshalBinary injects binary data to model
+func (c *Category) UnmarshalBinary(data []byte) error {
+	if err := json.Unmarshal(data, c); err != nil {
+		return exception.NewFieldFormat("category aggregate", "json")
+	}
+
+	return nil
 }
