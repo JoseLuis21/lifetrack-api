cb3f0066429d8ac20d9ff1ab35531561bab3bf2d
Alonso R <luis.alonso.16@hotmail.com>
2020-10-01 08:32:32 -0600

    Add remove category test

---
+++ internal/application/command/remove_category_test.go
@@ -0,0 +1,66 @@
+package command
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/alexandria-oss/common-go/exception"
+	"github.com/neutrinocorp/life-track-api/internal/domain/shared"
+	"github.com/neutrinocorp/life-track-api/internal/infrastructure"
+	"github.com/neutrinocorp/life-track-api/internal/infrastructure/eventbus"
+	"github.com/neutrinocorp/life-track-api/internal/infrastructure/persistence"
+	"testing"
+)
+
+func TestNewRemoveCategoryHandler(t *testing.T) {
+	cfg, err := infrastructure.NewConfiguration()
+	if err != nil {
+		t.Fatal("cannot start configuration")
+	}
+
+	r := persistence.NewCategoryInMemory()
+
+	cmdAdd := NewAddCategoryHandler(r, eventbus.NewInMemory(cfg))
+	err = cmdAdd.Invoke(AddCategory{
+		Ctx:         context.Background(),
+		Title:       "Classical Mechanics",
+		User:        "123",
+		Description: "",
+		Theme:       "red",
+	})
+	if err != nil {
+		t.Fatal("add category command failed", fmt.Sprintf("err: %v", exception.GetDescription(err)))
+	}
+
+	categories, _, err := r.Fetch(context.Background(), "", 1, shared.CategoryCriteria{})
+	if err != nil {
+		t.Fatal("add category command failed", fmt.Sprintf("err: %v", exception.GetDescription(err)))
+	}
+
+	t.Logf("add category command succeed")
+	t.Log(categories[0])
+
+	cmd := NewRemoveCategoryHandler(r, eventbus.NewInMemory(cfg))
+	err = cmd.Invoke(RemoveCategory{
+		Ctx: context.Background(),
+		ID:  "",
+	})
+	if err == nil {
+		t.Fatal("remove category command did not failed, expected required field (id)")
+	}
+
+	err = cmd.Invoke(RemoveCategory{
+		Ctx: context.Background(),
+		ID:  categories[0].ID,
+	})
+	if err != nil {
+		t.Fatal("remove category command failed", fmt.Sprintf("err: %v", exception.GetDescription(err)))
+	}
+
+	categories, _, err = r.Fetch(context.Background(), "", 1, shared.CategoryCriteria{})
+	if err != nil && !errors.Is(err, exception.NotFound) {
+		t.Fatal("remove category command failed, expected category not found")
+	}
+
+	t.Logf("remove category command succeed")
+}
