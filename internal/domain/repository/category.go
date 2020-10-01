455be220fc0ed2552b2d79736584f9b387ed9981
Alonso R <luis.alonso.16@hotmail.com>
2020-09-30 23:14:20 -0600

    Use type-safe criteria filter for category repo

--- internal/domain/repository/category.go
+++ internal/domain/repository/category.go
@@ -13,12 +13,12 @@ type Category interface {
 	// Save stores an aggregate.Category
 	Save(ctx context.Context, c aggregate.Category) error
 	// FetchByID returns an aggregate.Category by its value.UUID
-	FetchByID(ctx context.Context, id value.UUID) (*model.Category, error)
+	FetchByID(ctx context.Context, id value.CUID) (*model.Category, error)
 	// Fetch returns an aggregate.Category slice, accepts multiple filters and params such as nextToken and limit for
 	// pagination
-	Fetch(ctx context.Context, token string, limit int64, filter shared.FilterMap) ([]*model.Category, string, error)
+	Fetch(ctx context.Context, token string, limit int64, criteria shared.CategoryCriteria) ([]*model.Category, string, error)
 	// Replace mutates completely an aggregate.Category
 	Replace(ctx context.Context, c aggregate.Category) error
 	// HardRemove permanently removes an aggregate.Category
-	HardRemove(ctx context.Context, id value.UUID) error
+	HardRemove(ctx context.Context, id value.CUID) error
 }
--- internal/domain/shared/filter.go
+++ internal/domain/shared/filter.go
@@ -1,3 +1,11 @@
 package shared
 
+// FilterMap generic and non-type safe filter map
 type FilterMap map[string]string
+
+// CategoryCriteria represents a filter struct to fetch fine-grained categories
+type CategoryCriteria struct {
+	User  string `json:"user"`
+	Title string `json:"title"`
+	Query string `json:"query"`
+}
