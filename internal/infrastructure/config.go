03bc2d2dc625081b187e127ede81d8ebde524291
Alonso R <luis.alonso.16@hotmail.com>
2020-10-09 01:09:54 -0600

    Refactor kernel config, contains breaking changes
    Signed-off-by: Alonso R <luis.alonso.16@hotmail.com>

--- internal/infrastructure/config.go
+++ internal/infrastructure/config.go
@@ -2,60 +2,56 @@ package infrastructure
 
 import "github.com/spf13/viper"
 
+type dynamo struct {
+	Table  string
+	Region string
+}
+
+type persistence struct {
+	DynamoDB dynamo
+}
+
+type awsEvent struct {
+	Region string
+}
+
+type eventBus struct {
+	AWS awsEvent
+}
+
 type Configuration struct {
-	Category struct {
-		Table struct {
-			Name   string
-			Region string
-		}
-		Event struct {
-			Region string
-		}
-	}
+	Persistence persistence
+	EventBus    eventBus
 }
 
 func NewConfiguration() (Configuration, error) {
-	viper.SetDefault("category.table.name", "lt-category")
-	viper.SetDefault("category.table.region", "us-east-1")
-	viper.SetDefault("category.event.region", "us-east-1")
+	viper.SetDefault("lifetrack.persistence.dynamo.table", "lifetrack-prod")
+	viper.SetDefault("lifetrack.persistence.dynamo.region", "us-east-1")
+	viper.SetDefault("lifetrack.eventbus.aws.region", "us-east-1")
 
 	if err := SetOSEnv(); err != nil {
 		return Configuration{}, err
 	}
 
 	return Configuration{
-		Category: struct {
-			Table struct {
-				Name   string
-				Region string
-			}
-			Event struct {
-				Region string
-			}
-		}{
-			Table: struct {
-				Name   string
-				Region string
-			}{
-				Name:   viper.GetString("category.table.name"),
-				Region: viper.GetString("category.table.region"),
-			},
-			Event: struct {
-				Region string
-			}{
-				Region: viper.GetString("category.event.region"),
-			},
+		Persistence: persistence{DynamoDB: dynamo{
+			Table:  viper.GetString("lifetrack.persistence.dynamo.table"),
+			Region: viper.GetString("lifetrack.persistence.dynamo.region"),
+		},
+		},
+		EventBus: eventBus{
+			awsEvent{Region: viper.GetString("lifetrack.eventbus.aws.region")},
 		},
 	}, nil
 }
 
 func SetOSEnv() error {
 	viper.SetEnvPrefix("lt")
-	if err := viper.BindEnv("category_table_name", "category.table.name"); err != nil {
+	if err := viper.BindEnv("dynamo_table_name", "lifetrack.persistence.dynamo.table"); err != nil {
 		return err
-	} else if err := viper.BindEnv("category_event_region", "category.event.region"); err != nil {
+	} else if err := viper.BindEnv("event_aws_region", "lifetrack.eventbus.aws.region"); err != nil {
 		return err
 	}
 
-	return viper.BindEnv("category_table_region", "category.table.region")
+	return viper.BindEnv("dynamo_table_region", "lifetrack.persistence.dynamo.region")
 }
