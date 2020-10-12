3ef88c18fa347a28baa93aa8850e313361332a0e
Alonso R <luis.alonso.16@hotmail.com>
2020-10-12 02:36:13 -0600

    Add missing AWS Lambda category handlers
    Signed-off-by: Alonso R <luis.alonso.16@hotmail.com>

---
+++ cmd/remove-category/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/alexandria-oss/common-go/exception"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
+	"github.com/neutrinocorp/life-track-api/pkg/dep"
+	"github.com/neutrinocorp/life-track-api/pkg/transport/categoryhandler"
+)
+
+var muxLambda *gorillamux.GorillaMuxAdapter
+var cleaning func()
+
+func init() {
+	cmd, clean, err := dep.InjectRemoveCategory()
+	if err != nil {
+		log.Fatalf("failed to start remove category command: %s", exception.GetDescription(err))
+	}
+	cleaning = clean
+
+	h := categoryhandler.NewRemove(cmd, mux.NewRouter().PathPrefix("/live").Subrouter())
+	log.Print("category handler successfully started")
+	muxLambda = gorillamux.New(h.GetRouter())
+}
+
+// Handler AWS Lambda category handler
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return muxLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	defer cleaning()
+	lambda.Start(Handler)
+}
