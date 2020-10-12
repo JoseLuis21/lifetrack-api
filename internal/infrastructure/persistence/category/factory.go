6c1d0aa4af40d46eb5475cb8aecf5250749ef882
Alonso R <luis.alonso.16@hotmail.com>
2020-10-12 02:03:50 -0600

    Refactor category repo factory
    Signed-off-by: Alonso R <luis.alonso.16@hotmail.com>

--- internal/infrastructure/persistence/category/factory.go
+++ internal/infrastructure/persistence/category/factory.go
@@ -8,10 +8,8 @@ import (
 // NewCategory wraps an existing category repository with required observability and resiliency
 func NewCategory(r repository.Category, logger *zap.Logger) repository.Category {
 	// TODO: Add monitoring, distributed tracing and circuit-breaker w/ retry policy patterns
-	repo := Log{
+	return Log{
 		Log:  logger,
 		Next: r,
 	}
-
-	return repo
 }
