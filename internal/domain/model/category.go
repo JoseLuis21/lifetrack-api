5dba61a105548d7f241510dfc57d64b78177dfd7
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-18 16:49:34 -0600

    Add image field to category read model

--- internal/domain/model/category.go
+++ internal/domain/model/category.go
@@ -13,6 +13,7 @@ type Category struct {
 	Description string `json:"description,omitempty"`
 	User        string `json:"user"`
 	Color       string `json:"color,omitempty"`
+	Image       string `json:"image"`
 	CreateTime  int64  `json:"create_time"`
 	UpdateTime  int64  `json:"update_time"`
 	Active      bool   `json:"active"`
