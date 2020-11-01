cc83802e24d10879249af0e59a5eeeb6bd067e8a
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-31 21:00:04 -0700

    Override default logger with zap on cassandra factory

--- internal/infrastructure/persistence/dbpool/cassandra.go
+++ internal/infrastructure/persistence/dbpool/cassandra.go
@@ -2,15 +2,15 @@ package dbpool
 
 import (
 	"context"
-	"log"
 
 	"github.com/gocql/gocql"
 	"github.com/neutrinocorp/lifetrack-api/internal/infrastructure/configuration"
 	"go.uber.org/fx"
+	"go.uber.org/zap"
 )
 
 // NewCassandra creates an Apache Cassandra session
-func NewCassandra(lc fx.Lifecycle, cfg configuration.Configuration) (*gocql.Session, error) {
+func NewCassandra(lc fx.Lifecycle, logger *zap.Logger, cfg configuration.Configuration) (*gocql.Session, error) {
 	cluster := gocql.NewCluster(cfg.Cassandra.Cluster...)
 	cluster.Authenticator = gocql.PasswordAuthenticator{
 		Username: cfg.Cassandra.Username,
@@ -28,7 +28,10 @@ func NewCassandra(lc fx.Lifecycle, cfg configuration.Configuration) (*gocql.Sess
 			return nil
 		},
 		OnStop: func(ctx context.Context) error {
-			log.Print("closing cassandra session")
+			logger.Info("closing cassandra session",
+				zap.String("ip_address", cfg.Cassandra.Cluster[0]),
+				zap.String("keyspace", cfg.Cassandra.Keyspace),
+			)
 			session.Close()
 			return nil
 		},
