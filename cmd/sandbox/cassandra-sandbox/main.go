8bc74e9a60b0970c2463316a85ce94e50b30ecc4
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-31 20:58:26 -0700

    Update cassandra sandbox

--- cmd/sandbox/cassandra-sandbox/main.go
+++ cmd/sandbox/cassandra-sandbox/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"github.com/alexandria-oss/common-go/exception"
+	"github.com/neutrinocorp/lifetrack-api/internal/domain/aggregate"
 	"log"
 
 	"github.com/gocql/gocql"
@@ -9,6 +11,7 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/neutrinocorp/lifetrack-api/internal/infrastructure/configuration"
+	"github.com/neutrinocorp/lifetrack-api/internal/infrastructure/logging"
 	"github.com/neutrinocorp/lifetrack-api/internal/infrastructure/persistence/dbpool"
 )
 
@@ -16,6 +19,7 @@ func main() {
 	app := fx.New(
 		fx.Provide(
 			configuration.NewConfiguration,
+			logging.NewZap,
 			dbpool.NewCassandra,
 		),
 		fx.Invoke(run),
@@ -33,18 +37,17 @@ func main() {
 }
 
 func run(s *gocql.Session) {
-
 	log.Print("on workflow")
-	/*
-			c, err := aggregate.NewCategory("aruiz", "Gaming")
-			if err != nil {
-				log.Fatal(exception.GetDescription(err))
-			}
-			_ = c.ModifyDescription("Gaming")
 
-			if err = s.Query(`INSERT INTO category (id, user_id, name, description,
+	c, err := aggregate.NewCategory("aruiz", "Gaming")
+	if err != nil {
+		log.Fatal(exception.GetDescription(err))
+	}
+	_ = c.ModifyDescription("Gaming")
+
+	if err = s.Query(`INSERT INTO category (id, user_id, name, description,
 		    target_time, picture, create_time, update_time, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-				c.ID(), c.User(), c.Name(), c.Description(), int64(c.TargetTime().Minutes()), c.Picture(), c.CreateTime(), c.UpdateTime(), c.State()).Exec(); err != nil {
-				log.Fatal(err)
-			}*/
+		c.ID(), c.User(), c.Name(), c.Description(), int64(c.TargetTime().Minutes()), c.Picture(), c.CreateTime(), c.UpdateTime(), c.State()).Exec(); err != nil {
+		log.Fatal(err)
+	}
 }
