a03a97d833e09c6c1318ba65c059b5c075e4b75a
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-31 20:58:52 -0700

    Add uber zap logger implementation

--- internal/infrastructure/logging/zap.go
+++ internal/infrastructure/logging/zap.go
@@ -1,4 +1,28 @@
 package logging
 
-func NewZap() {
+import (
+	"context"
+	"log"
+
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+func NewZap(lc fx.Lifecycle) *zap.Logger {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Printf("can't initialize zap logger %+v", err)
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			return err
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Info("closing zap logger")
+			return logger.Sync()
+		},
+	})
+
+	return logger
 }
