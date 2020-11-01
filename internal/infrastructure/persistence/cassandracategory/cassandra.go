1f88425b935ef325032083dec4faff0c398a600c
A Ruiz <luis.alonso.16@hotmail.com>
2020-10-31 22:10:33 -0700

    Add default cassandra repository

--- internal/infrastructure/persistence/cassandracategory/cassandra.go
+++ internal/infrastructure/persistence/cassandracategory/cassandra.go
@@ -1,1 +1,43 @@
 package cassandracategory
+
+import (
+	"context"
+	"github.com/gocql/gocql"
+	"github.com/neutrinocorp/lifetrack-api/internal/domain/aggregate"
+	"github.com/neutrinocorp/lifetrack-api/internal/domain/repository"
+	"sync"
+)
+
+type Repository struct {
+	s  *gocql.Session
+	mu *sync.RWMutex
+}
+
+// Singleton is required in this layer to keep mutex consistency between instances
+var (
+	singleton     *Repository
+	singletonLock = new(sync.Once)
+)
+
+func NewRepository(s *gocql.Session) *Repository {
+	singletonLock.Do(func() {
+		singleton = &Repository{
+			s:  s,
+			mu: new(sync.RWMutex),
+		}
+	})
+
+	return singleton
+}
+
+func (r Repository) Save(ctx context.Context, category aggregate.Category) error {
+	panic("not implemented")
+}
+
+func (r Repository) Fetch(ctx context.Context, criteria repository.CategoryCriteria) ([]*aggregate.Category, string, error) {
+	panic("not implemented")
+}
+
+func (r Repository) Remove(ctx context.Context, id string) error {
+	panic("not implemented")
+}
